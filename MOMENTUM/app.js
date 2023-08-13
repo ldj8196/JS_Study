c22b5da2abf21a4cc66760db60f8323eade70f78
이동준 <ldj8196@naver.com>
2023-08-13 19:37:59 +0900

    2023-08-13

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,18 +1,29 @@
-const title = document.querySelector(".hello:first-child h1");
 
-function handleTitleClick() {
-    title.style.color = "blue";
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username);
 }
 
-function handleMouseEnter() {
-    title.innerText = "Mouse is Here!";
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME); 
 }
 
-function handleMouseLeave() {
-    title.innerText = "Mouse is Gone!";
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting(savedUsername);
 }
-
-
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
